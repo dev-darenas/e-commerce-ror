93e2a9a43787e5692b279e4860e92fa192291cbc
AndxParedes <gparedesalzate@gmail.com>
2020-06-18 17:34:13 -0500

    option types

--- app/controllers/option_types_controller.rb
+++ app/controllers/option_types_controller.rb
@@ -1,53 +1,53 @@
 class OptionTypesController < ApplicationController
     def index
-        @option_type = OptionType.all.order("created_at DESC")
+        @option_types = @store.option_types.all.order("created_at DESC")
     end
     def new
-        @option_type = OptionType.new
+        @option_types = @store.option_types.new
     end
     def show
-        @option_type = OptionType.find(params[:id])
+        @option_types = @store.option_types.find(params[:id])
     end
     def edit
-        @option_type = OptionType.find(params[:id])
+        @option_types = @store.option_types.find(params[:id])
     end
     def create
-        @option_type = OptionType.new(option_type_params)
+        @option_types = @store.option_types.new(option_types_params)
         respond_to do |format|
-            if @option_type.save
-              format.html { redirect_to @option_type, notice: 'Option_type was successfully created.' }
-              format.json { render :show, status: :created, location: @option_type }
+            if @option_types.save
+              format.html { redirect_to @option_types, notice: 'option_types was successfully created.' }
+              format.json { render :show, status: :created, location: @option_types }
             else
               format.html { render :new }
-              format.json { render json: @option_type.errors, status: :unprocessable_entity }
+              format.json { render json: @option_types.errors, status: :unprocessable_entity }
             end
           end
     end
     def update
         respond_to do |format|
-          if @option_type.update(option_type_params)
-            format.html { redirect_to @option_type, notice: 'Option type was successfully updated.' }
-            format.json { render :show, status: :ok, location: @option_type }
+          if @option_types.update(option_types_params)
+            format.html { redirect_to @option_types, notice: 'Option type was successfully updated.' }
+            format.json { render :show, status: :ok, location: @option_types }
           else
             format.html { render :edit }
-            format.json { render json: @option_type.errors, status: :unprocessable_entity }
+            format.json { render json: @option_types.errors, status: :unprocessable_entity }
           end
         end
-        @option_type = OptionType.find(params[:id])
+        @option_types = @store.option_types.find(params[:id])
  
-        if @option_type.update(option_type_params)
-          redirect_to @option_type
+        if @option_types.update(option_types_params)
+          redirect_to @option_types
         else
           render 'edit'
         end
     end
     def destroy
-        @option_type = OptionType.find(params[:id])
-        @option_type.destroy
+        @option_types = @store.option_types.find(params[:id])
+        @option_types.destroy
  
         redirect_to option_types_url
     end
-    def option_type_params
-        params.require(:option_type).permit(:name, :presentation)
+    def option_types_params
+        params.require(:option_types).permit(:name, :presentation)
     end
 end
